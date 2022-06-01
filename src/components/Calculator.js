61a2e35ef52984ea0476b6fee7bf9108bf853f2e
unknown <ibr.s.ahmat@gmail.com>
2022-06-01 20:26:55 +0300

    Created src folder and th Calculator.js file

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+/* eslint-disable */
+class Calculator extends React.Component {
+        render() {
+            return ( < div className = "contianer" >
+                <
+                /div>);
+            }
+        }
+
+        export default Calculator;
