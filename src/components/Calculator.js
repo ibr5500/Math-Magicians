c79d8de6cb1fe001dda189fbf9d7b01ef9dcadef
unknown <ibr.s.ahmat@gmail.com>
2022-06-02 23:41:41 +0300

    Create the UI for the Calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,43 @@
 import React from 'react';
+
 /* eslint-disable */
 class Calculator extends React.Component {
-        render() {
-            return ( < div className = "contianer" >
-                <
-                /div>);
-            }
-        }
+    render() {
+        return ( 
+            <div className = "contianer">
+                <div className='res'> 0 </div>
+                <div className='row'>
+                    <button type="button">AC</button>
+                    <button type="button">+/-</button>
+                    <button type="button">%</button>
+                    <button type="button" className='ops'>÷</button>
+                </div>
+                <div className='row'>
+                    <button type="button">7</button>
+                    <button type="button">8</button>
+                    <button type="button">9</button>
+                    <button type="button" className='ops'>x</button>
+                </div>
+                <div className='row'>
+                    <button type="button">4</button>
+                    <button type="button">5</button>
+                    <button type="button">6</button>
+                    <button type="button" className='ops'>-</button>
+                </div>
+                <div className='row'>
+                    <button type="button">1</button>
+                    <button type="button">2</button>
+                    <button type="button">3</button>
+                    <button type="button" className='ops'>+</button>
+                </div>
+                <div className='row'>
+                    <button type="button" className='zero'>0</button>
+                    <button type="button">.</button>
+                    <button type="button" className='ops'>=</button>
+                </div>
+            </div>
+        );
+    }
+}
 
-        export default Calculator;
+export default Calculator;
