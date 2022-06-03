e0274dd3ee1b1bdbec903cf5bbffed22a125aae7
unknown <ibr.s.ahmat@gmail.com>
2022-06-03 21:56:54 +0300

    Reafactor the Calculator component to a function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,65 +1,58 @@
-import React from 'react';
+import React, { useState }from 'react';
 import calculate from './logic/calculate';
 
-/* eslint-disable */
-class Calculator extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            total: 0,
-            next: null,
-            operation: null,
-        };
-        this.handleClick = this.handleClick.bind(this);
-    }
 
-    handleClick(event) {
-        this.setState((state) => {
-            return calculate(state, event.target.id);
-        });
-    }
+const Calculator = () => {
+    const [anyName, setState] = useState({
+        total: 0,
+        next: null,
+        operation: null,
+    });
 
-    render() {
-        const {total, next, operation} = this.state; 
-        return ( 
-            <div className = "contianer">
-                <div className='res'>
-                    <span>{total}</span>
-                    <span>{next}</span>
-                    <span>{operation}</span> 
-                </div>
-                <div className='row'>
-                    <button type="button" id='AC' onClick={this.handleClick}>AC</button>
-                    <button type="button" id='+/-' onClick={this.handleClick}>+/-</button>
-                    <button type="button" id='%' onClick={this.handleClick}>%</button>
-                    <button type="button" id='÷' className='ops' onClick={this.handleClick}>÷</button>
-                </div>
-                <div className='row'>
-                    <button type="button" id='7' onClick={this.handleClick}>7</button>
-                    <button type="button" id='8' onClick={this.handleClick}>8</button>
-                    <button type="button" id='9' onClick={this.handleClick}>9</button>
-                    <button type="button" id='x' className='ops' onClick={this.handleClick}>x</button>
-                </div>
-                <div className='row'>
-                    <button type="button" id='4' onClick={this.handleClick}>4</button>
-                    <button type="button" id='5' onClick={this.handleClick}>5</button>
-                    <button type="button" id='6' onClick={this.handleClick}>6</button>
-                    <button type="button" id='-' className='ops' onClick={this.handleClick}>-</button>
-                </div>
-                <div className='row'>
-                    <button type="button" id='1' onClick={this.handleClick}>1</button>
-                    <button type="button" id='2' onClick={this.handleClick}>2</button>
-                    <button type="button" id='3' onClick={this.handleClick}>3</button>
-                    <button type="button" id='+' className='ops' onClick={this.handleClick}>+</button>
-                </div>
-                <div className='row'>
-                    <button type="button" id='0' className='zero' onClick={this.handleClick}>0</button>
-                    <button type="button" id='.' onClick={this.handleClick}>.</button>
-                    <button type="button" id='=' className='ops' onClick={this.handleClick}>=</button>
-                </div>
-            </div>
+    const handleClick = (event) => {
+        setState(
+            calculate(anyName, event.target.id)
         );
-    }
-}
+    };
+
+    const {total, next, operation} = anyName;
+    
+    return ( 
+        <div className = "contianer">
+            <div className='res'>
+                {total} {operation} {next}
+            </div>
+            <div className='row'>
+                <button type="button" id='AC' onClick={handleClick}>AC</button>
+                <button type="button" id='+/-' onClick={handleClick}>+/-</button>
+                <button type="button" id='%' onClick={handleClick}>%</button>
+                <button type="button" id='÷' className='ops' onClick={handleClick}>÷</button>
+            </div>
+            <div className='row'>
+                <button type="button" id='7' onClick={handleClick}>7</button>
+                <button type="button" id='8' onClick={handleClick}>8</button>
+                <button type="button" id='9' onClick={handleClick}>9</button>
+                <button type="button" id='x' className='ops' onClick={handleClick}>x</button>
+            </div>
+            <div className='row'>
+                <button type="button" id='4' onClick={handleClick}>4</button>
+                <button type="button" id='5' onClick={handleClick}>5</button>
+                <button type="button" id='6' onClick={handleClick}>6</button>
+                <button type="button" id='-' className='ops' onClick={handleClick}>-</button>
+            </div>
+            <div className='row'>
+                <button type="button" id='1' onClick={handleClick}>1</button>
+                <button type="button" id='2' onClick={handleClick}>2</button>
+                <button type="button" id='3' onClick={handleClick}>3</button>
+                <button type="button" id='+' className='ops' onClick={handleClick}>+</button>
+            </div>
+            <div className='row'>
+                <button type="button" id='0' className='zero' onClick={handleClick}>0</button>
+                <button type="button" id='.' onClick={handleClick}>.</button>
+                <button type="button" id='=' className='ops' onClick={handleClick}>=</button>
+            </div>
+        </div>
+    );
+};
 
 export default Calculator;
