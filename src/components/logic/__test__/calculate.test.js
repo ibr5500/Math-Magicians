98dd938bd7feb2840833440b16308bdd941e15b5
Talha <tkz9696@gmail.com>
2022-06-09 13:54:07 +0500

    Correct test for calculate.js

--- src/components/logic/__test__/calculate.test.js
+++ src/components/logic/__test__/calculate.test.js
@@ -2,8 +2,11 @@ import calculate from '../calculate';
 
 describe('Tests for calculate.js', () => {
     test('Any number should become 0 when AC is pressed', () => {
-        expect(calculate('5', 'AC')).toBe('0');
-      })
+        const obj = {
+            total: 0,
+            next: null,
+            operation: null,
+        };
+        expect(calculate(obj, 'AC').total).toEqual(0);
+    });
 });
-
-// calculate.total === 0
